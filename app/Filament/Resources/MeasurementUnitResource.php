fc8f24031ea170fdf52e62a8d172e6c903b8e918
Angel Pimentel <angelpimentelcontact@gmail.com>
2025-01-12 11:56:12 -0400

    Add filament measurement unit resource

---
+++ app/Filament/Resources/MeasurementUnitResource.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MeasurementUnitResource\Pages;
+use App\Filament\Resources\MeasurementUnitResource\RelationManagers;
+use App\Models\MeasurementUnit;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class MeasurementUnitResource extends Resource
+{
+    protected static ?string $model = MeasurementUnit::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required(),
+                Forms\Components\Textarea::make('description')
+                    ->label('Description')
+                    ->nullable(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->label('Name')->sortable()->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMeasurementUnits::route('/'),
+            'create' => Pages\CreateMeasurementUnit::route('/create'),
+            'edit' => Pages\EditMeasurementUnit::route('/{record}/edit'),
+        ];
+    }
+}
